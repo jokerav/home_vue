69e5fc9c7d727179e4b7018f3aac930865ce8627
jokerav <jokeravdeveloper@gmail.com>
2022-10-30 19:40:03 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<Button type="button">Click me</Button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Button from "./components/Button";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Button
   }
 }
 </script>
---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <button><slot></slot></button>
+</template>
+
+<script>
+    export default {
+        name: "Button",
+        props:{
+            type: {
+                type: String,
+                 default: 'button'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
